4db8515b84216b829ede1a4e555b76344b0c4840
Christopher Lai <christopherliamlai@me.com>
2020-04-15 16:29:45 -0700

    Add remaining Address API calls

--- lib/shippo/operation/address.ex
+++ lib/shippo/operation/address.ex
@@ -1,8 +1,12 @@
 defmodule Shippo.Operation.Address do
+  @moduledoc """
+  [Shippo Addresses](https://goshippo.com/docs/reference#addresses)
+  """
+
   alias Shippo.Operation
 
   @doc """
-  Returns an `Operation` struct to create a new Address.
+  Creates a new address object.
   """
   @spec create(params :: map(), opts :: keyword()) :: Operation.t()
   def create(params, _opts \\ []) do
@@ -10,7 +14,28 @@ defmodule Shippo.Operation.Address do
   end
 
   @doc """
-  Returns an `Operation` struct for listing all Addresses.
+  Retrieve an existing address by object id.
+
+  """
+  @spec get(id :: binary(), opts :: keyword()) :: Operation.t()
+  def get(id, opts \\ []) do
+    path = Enum.join(["/addresses", id], "/")
+
+    Operation.new(:get, path, nil, opts)
+  end
+
+  @doc """
+  Validate an existing address by object id.
+  """
+  @spec validate(id :: binary(), opts :: keyword()) :: Operation.t()
+  def validate(id, opts \\ []) do
+    path = Enum.join(["/addresses", id, "validate"], "/")
+
+    Operation.new(:get, path, nil, opts)
+  end
+
+  @doc """
+  List all addresses.
   """
   @spec list :: Operation.t()
   def list(opts \\ []) do
