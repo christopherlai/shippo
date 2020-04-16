44bb12d6d4bfdccbcd233620e49b860a3c194185
Christopher Lai <christopherliamlai@me.com>
2020-04-15 17:10:27 -0700

    Add Refund operations

---
+++ lib/shippo/operation/refund.ex
@@ -0,0 +1,33 @@
+defmodule Shippo.Operation.Refund do
+  @moduledoc """
+  [Shippo Refunds](https://goshippo.com/docs/reference#refunds)
+  """
+
+  alias Shippo.Operation
+
+  @doc """
+  Creates a new refund object.
+  """
+  @spec create(params :: map(), opts :: keyword()) :: Operation.t()
+  def create(params, _opts \\ []) do
+    Operation.new(:post, "/refunds", params)
+  end
+
+  @doc """
+  Retrieve an existing refund by object id.
+  """
+  @spec get(id :: binary(), opts :: keyword()) :: Operation.t()
+  def get(id, opts \\ []) do
+    path = Enum.join(["/refunds", id], "/")
+
+    Operation.new(:get, path, nil, opts)
+  end
+
+  @doc """
+  List all refund objects.
+  """
+  @spec list :: Operation.t()
+  def list(opts \\ []) do
+    Operation.new(:get, "/refunds", nil, opts)
+  end
+end
