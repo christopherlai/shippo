0d2c5bc00217d598e94250b978f81edaa8472d0f
Christopher Lai <christopherliamlai@me.com>
2020-04-15 17:31:31 -0700

    Add Order operations

---
+++ lib/shippo/operation/order.ex
@@ -0,0 +1,33 @@
+defmodule Shippo.Operation.Order do
+  @moduledoc """
+  [Shippo Order](https://goshippo.com/docs/reference#parcels)
+  """
+
+  alias Shippo.Operation
+
+  @doc """
+  Creates a new order object.
+  """
+  @spec create(params :: map(), opts :: keyword()) :: Operation.t()
+  def create(params, _opts \\ []) do
+    Operation.new(:post, "/orders", params)
+  end
+
+  @doc """
+  Retrieve an existing order by object id.
+  """
+  @spec get(id :: binary(), opts :: keyword()) :: Operation.t()
+  def get(id, opts \\ []) do
+    path = Enum.join(["/orders", id], "/")
+
+    Operation.new(:get, path, nil, opts)
+  end
+
+  @doc """
+  List all order objects.
+  """
+  @spec list :: Operation.t()
+  def list(opts \\ []) do
+    Operation.new(:get, "/orders", nil, opts)
+  end
+end
